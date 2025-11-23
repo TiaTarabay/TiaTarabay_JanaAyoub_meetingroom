09021f868ff597c2f0c34c732f0584b922d3d559
TiaTarabay <twt00@mail.aub.edu>
2025-11-23 14:16:41 +0200

    Delete users_service directory

--- users_service/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .routes import router
-from .db import Base, engine
-
-app = FastAPI(title="Users Service")
-
-Base.metadata.create_all(bind=engine)
-
-app.include_router(router, prefix="/users")
